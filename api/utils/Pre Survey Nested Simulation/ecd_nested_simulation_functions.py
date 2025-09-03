0d90aaa579e583bf4b0bb5789978740d1ec1caba
amrita112 <amsi.1994@gmail.com>
2025-09-03 10:20:46 -0400

    Sanity check of WHZ simulation
    Testing whether simulated WHZ values match data, with % stunting (HAZ < -2) and % underweight (WAZ < -2) as inputs to simulation, based on data

--- api/utils/Pre Survey Nested Simulation/ecd_nested_simulation_functions.py
+++ api/utils/Pre Survey Nested Simulation/ecd_nested_simulation_functions.py
@@ -18,7 +18,8 @@ def generate_real_measurements(
     max_age,
     haz_params,
     waz_params,
-    whz_params,
+    whz_params_lying,
+    whz_params_standing,
     haz_mean_0 = None,
     haz_std_0 = None,
     waz_mean_0 = None,
@@ -30,6 +31,7 @@ def generate_real_measurements(
     min_weight=None,
     max_weight=None,
     plot_distributions = False,
+    figsize = [10, 10]
     
 ):
     """
@@ -64,6 +66,7 @@ def generate_real_measurements(
     # If haz_mean_0 and haz_std_0 are provided, use them to generate initial haz distribution
     if haz_mean_0 is not None and haz_std_0 is not None:
         haz_0 = np.random.normal(loc=haz_mean_0, scale=haz_std_0, size=num_children)
+        percent_stunting = (haz_0 < -2).mean()*100
     elif percent_stunting is not None:
         # If percent_stunting is provided, use it to calculate haz_mean_0, assuming standard deviation of 1. 
         # Percent stunting is the percentile of the distribution that is less than two standard deviations from the mean, i.e. < -2.
@@ -79,6 +82,7 @@ def generate_real_measurements(
     # If waz_mean_0 and waz_std_0 are provided, use them to generate initial waz distribution
     if waz_mean_0 is not None and waz_std_0 is not None:
         waz_0 = np.random.normal(loc=waz_mean_0, scale=waz_std_0, size=num_children)
+        percent_underweight = (waz_0 < -2).mean()*100
     elif percent_underweight is not None:
         # If percent_underweight is provided, use it to calculate waz_mean_0, assuming standard deviation of 1. 
         # Percent underweight is the percentile of the distribution that is less than two standard deviations from the mean, i.e. < -2.
@@ -99,8 +103,11 @@ def generate_real_measurements(
     if max_weight is not None:
         weights_0 = np.minimum(weights_0, max_weight)
 
+    # Determine length or height measurement based on age (loh = 1 for lying if age < 2 years, else loh = 2 for standing)
+    loh = pd.Series(np.where(start_ages < 730, 1, 2))  # 730 days = 2 years
+
     # Generate WHZ from height and weight
-    whz_0 = calculate_whz(pd.Series(heights_0), pd.Series(weights_0), genders, whz_params)
+    whz_0 = calculate_whz(pd.Series(heights_0), pd.Series(weights_0), pd.Series(genders), loh, whz_params_lying, whz_params_standing)
     percent_wasting = (whz_0 < -2).mean() * 100
     print('{0}% wasting'.format(percent_wasting))
 
@@ -112,14 +119,19 @@ def generate_real_measurements(
     'age': start_ages,
     'height': heights_0,
     'weight': weights_0,
+    'loh': loh,
     'haz': haz_0,
     'waz': waz_0,
-    'whz': whz_0
-})
-
+    'whz': whz_0,
+    'percent_stunting': percent_stunting,
+    'percent_underweight': percent_underweight,
+    'percent_wasting': percent_wasting
+    
+    })
+    
     # Plot age, haz, waz, whz, height and weight distributions if plot_distributions is True
     if plot_distributions:
-        plt.figure(figsize=(12, 8), constrained_layout = True)
+        plt.figure(figsize=figsize, constrained_layout = True)
 
         plt.subplot(2, 3, 1)
         sns.histplot(real_measurements['age']/365, bins=30, kde=True, color = 'lightgray')
@@ -135,10 +147,12 @@ def generate_real_measurements(
 
         plt.subplot(2, 3, 4)
         sns.histplot(real_measurements['height'], bins=30, kde=True, color = 'turquoise')
+        plt.xlim([40, 140])
         plt.title('Height (cm)')
 
         plt.subplot(2, 3, 5)
         sns.histplot(real_measurements['weight'], bins=30, kde=True, color = 'salmon')
+        plt.xlim([0, 30])
         plt.title('Weight (kg)')
 
         plt.subplot(2, 3, 6)
@@ -151,6 +165,58 @@ def generate_real_measurements(
     return real_measurements
 
 def generate_L0_distorted_measurements(
+        real_measurements, 
+        percent_under_reporting_stunting = None,
+        percent_under_reporting_underweight = None,
+        percent_under_reporting_wasting = None,
+        reporting_threshold = -2,
+        n_bins = 10,
+        ):
+
+    """
+    Apply incentive and capacity-based distortions, and measurement error, to real data to generate distorted L0 data.
+    Args:
+        real_measurements (pd.DataFrame): Table with columns
+        percent_under_reporting_stunting (float): Percentage of stunted children who are reported as non-stunted.
+        percent_under_reporting_underweight (float): Percentage of underweight children who are reported as non
+        percent_under_reporting_wasting (float): Percentage of wasted children who are reported as non-wasted.
+        reporting_threshold (float): Z-score threshold for reporting (default is -2).
+        n_bins (int): Number of bins to use for under-reporting distribution (default is 10).
+    Returns:
+        pd.DataFrame: Table with distorted measurements and reported status.
+    """
+    # Check that percent under reporting are provided either for stunting and underweight or wasting, and if not throw an error.
+    if (percent_under_reporting_stunting is None or percent_under_reporting_underweight is None) and percent_under_reporting_wasting is None:
+        raise ValueError("Percent under-reporting must be provided for either stunting and underewight, or wasting.")
+
+    distorted_measurements = real_measurements.copy()
+
+    # Apply under-reporting for stunting and underweight if provided
+    if percent_under_reporting_stunting is not None and percent_under_reporting_underweight is not None:
+        # Under-reporting for stunting
+        stunted_mask = distorted_measurements['haz'] < reporting_threshold
+        num_stunted = stunted_mask.sum()
+        num_to_report_as_non_stunted = int(num_stunted * percent_under_reporting_stunting / 100)
+
+        if num_to_report_as_non_stunted > 0:
+            
+            stunted_haz_values = distorted_measurements.loc[stunted_mask, 'haz']
+            bins = np.linspace(stunted_haz_values.min(), reporting_threshold, n_bins + 1)
+            bin_indices = np.digitize(stunted_haz_values, bins) - 1
+            
+            # Create bins above the reporting threshold to which we can move the selected children
+            bins_above_threshold = np.linspace(reporting_threshold, np.max(distorted_measurements['haz']), n_bins + 1)
+
+            # To simulate bunching at the threhsold, assign decreasing weights to each bin above the threshold
+            bin_weights = np.linspace(1, 0.1, n_bins)
+
+            # In each bin below the threshold, select percent_under_reporting_stunting% of the children to be reported as non-stunted 
+            # and change their haz value to a bin above the reporting threshold (-2). Choose the bin above the threshold based on the weights defined above.
+                     
+
+
+            distorted_measurements.loc[selected_indices, 'haz'] = reporting_threshold + 0.01
+
 
 def calculate_haz(height, age, sex, haz_params):
     """
@@ -242,7 +308,7 @@ def weight_from_waz(waz, age, sex, waz_params):
     weight = invert_anthro_zscore(waz, m, s, l)
     return weight
 
-def calculate_whz(height, weight, sex, whz_params):
+def calculate_whz(height, weight, sex, loh, whz_params_lying, whz_params_standing):
     """
     Calculate Weight-for-Height Z-scores (WHZ) using the WHO growth standards.
 
@@ -250,22 +316,42 @@ def calculate_whz(height, weight, sex, whz_params):
         height (pd.Series): Height measurements.
         weight (pd.Series): Weight measurements.
         age (pd.Series): Age measurements.
+        loh (pd.Series): Whether height was measured standing or lying down.
+        whz_params_lying (pd.DataFrame): WHZ parameters from WHO growth standards for heights measured lying down.
+        whz_params_standing (pd.DataFrame): WHZ parameters from WHO growth standards for heights measured standing up.
 
     Returns:
         pd.Series: WHZ scores.
     """
-    # Make sure height, weight, age and sex are the same length, else throw an error
-    if not (len(height) == len(weight) == len(sex)):
+    # Make sure height, weight, age, loh and sex are the same length, else throw an error
+    if not (len(height) == len(weight) == len(loh) == len(sex)):
         raise ValueError("Input series must have the same length")
     
-    # Get M, S, L values for the age and sex
-    # Create a variable called height_adjusted in which each value from height_0 is replaced with the closest value from whz_params['__000003']
-    height_array = np.array(whz_params['__000003'])
-    print('Calculating adjusted heights')
-    height_adjusted = pd.Series(height_array[np.abs(height_array[:, None] - height.values).argmin(axis=0)]) 
-    print('Adjusted heights calculated')
-    m, s, l = get_msl(pd.Series(height_adjusted), sex, whz_params, age_label = '__000003')
-    whz = get_anthro_zscore(weight.dropna(), m, s, l)
+    # Split height, weight and sex into two groups based on loh
+    height_0 = height[loh == 1]  # Lying height
+    weight_0 = weight[loh == 1] # Weight corresponding to lying height
+    sex_0 = sex[loh == 1] # Sex corresponding to lying height
+
+    height_1 = height[loh == 2]  # Standing height
+    weight_1 = weight[loh == 2] # Weight corresponding to standing height
+    sex_1 = sex[loh == 2] # Sex corresponding to standing height
+
+    # Get M, S, L values for the age and sex for lying height
+    # Create a variable called height_adjusted in which each value from height_0 is replaced with the closest value from whz_params_lying['__000002']
+    height_array = np.array(whz_params_lying['__000002'])
+    height_adjusted = pd.Series(height_array[np.abs(height_array[:, None] - height_0.values).argmin(axis=0)]) 
+    m, s, l = get_msl(pd.Series(height_adjusted), sex_0, whz_params_lying, age_label = '__000002')
+    whz_0 = get_anthro_zscore(weight_0.dropna(), m, s, l)
+
+    # Get M, S, L values for the age and sex for standing height
+    # Create a variable called height_adjusted in which each value from height_1 is replaced with the closest value from whz_params_standing['__000003']
+    height_array = np.array(whz_params_standing['__000003'])
+    height_adjusted = pd.Series(height_array[np.abs(height_array[:, None] - height_1.values).argmin(axis=0)]) 
+    m, s, l = get_msl(pd.Series(height_adjusted), sex_1, whz_params_standing, age_label = '__000003')
+    whz_1 = get_anthro_zscore(weight_1.dropna(), m, s, l)
+    
+    # Return combined whz values
+    whz = pd.concat([whz_0, whz_1]).sort_index()
     return whz
 
 def get_anthro_zscore(y, m, s, l):
